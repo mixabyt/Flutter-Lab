378c45d1bdfa489952af81d9a105f73ce3f38655
mixabyt <maksnot375@gmail.com>
2025-11-04 21:38:19 +0200

    lab9

--- lib/reset_password_screen.dart
+++ lib/reset_password_screen.dart
@@ -1,83 +1,108 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:email_validator/email_validator.dart';
 class ResetPasswordScreen extends StatefulWidget {
   @override
   State<ResetPasswordScreen> createState() => _ResetPasswordScreenState();
 }
 
 class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
   final nameController = TextEditingController();
 
-  bool show = false;
-
+@override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(child: Text("Reset password", style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 32),)),
-              SizedBox(height: 20),
-               Text(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    "Reset password",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium!
+                        .copyWith(fontSize: 32),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
                   'Email',
-                  style: Theme.of(context)
-                    .textTheme
-                    .labelSmall
+                  style: Theme.of(context).textTheme.labelSmall,
                 ),
-             TextField(
-              controller: nameController,
-            ),
-            show ? Text("email field can not be empty", style: TextStyle(color: Colors.red),) : Container(),
-              SizedBox(height: 10),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {});
-                  if (nameController.text.isEmpty) {
-                    show = true; 
-                    return;
-                  }
-                  AlertDialog alert = AlertDialog(
-                    title: Text("Reset Password"),
-                    content: Text("Password reset link sent to your email"),
-                  );
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return alert;
+                TextFormField(
+                  controller: nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "email field can not be empty";
+                    }else if (!EmailValidator.validate(value)) {
+                      return 'Enter a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        AlertDialog alert = AlertDialog(
+                          title: const Text("Reset Password"),
+                          content: const Text(
+                              "Password reset link sent to your email"),
+                        );
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return alert;
+                          },
+                        );
+                        nameController.clear();
+                      }
                     },
-                  );
-                  show = false;
-                  nameController.clear();
-                  
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                    ),
+                    child: Text(
+                      'Reset password',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelMedium!
+                          .copyWith(color: Colors.blue),
+                    ),
+                  ),
                 ),
-                child: Text('Reset password', style: Theme.of(context).textTheme.labelMedium!.copyWith(color: Colors.blue),)
-              ),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  
-                  backgroundColor: Colors.white,
+                const SizedBox(height: 10),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                    ),
+                    child: Text(
+                      'Back',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelMedium!
+                          .copyWith(color: Colors.blue),
+                    ),
+                  ),
                 ),
-                child: Text('Back', style: Theme.of(context).textTheme.labelMedium!.copyWith(color: Colors.blue),)
-              ),
+              ],
             ),
-            SizedBox(height: 10),
-             
-            ],
           ),
         ),
       ),
--- lib/sign_in_screen.dart
+++ lib/sign_in_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 import 'sign_up_screen.dart';
 import 'reset_password_screen.dart';
-class SignInScreen extends StatefulWidget {
 
+class SignInScreen extends StatefulWidget {
   SignInScreen({super.key});
 
   @override
@@ -10,12 +11,31 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-  TextEditingController nameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
-  TextEditingController nameController1 = TextEditingController();
+  final nameController = TextEditingController();
+  final nameController1 = TextEditingController();
 
-  bool nameControllerEmpty = false;
-  bool nameController1Empty = false;
+
+  @override
+  void initState() {
+    nameController.clear();
+    nameController1.clear();  
+    super.initState();
+  }
+
+@override
+  void dispose() {
+    nameController.dispose();
+    nameController1.dispose();
+    super.dispose();
+  }
+
+  void _resetForm() {
+    _formKey.currentState?.reset();
+    nameController.clear();
+    nameController1.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,130 +43,136 @@ class _SignInScreenState extends State<SignInScreen> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(child: Image.network('https://i.guim.co.uk/img/media/327aa3f0c3b8e40ab03b4ae80319064e401c6fbc/377_133_3542_2834/master/3542.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=34d32522f47e4a67286f9894fc81c863', height: 100, )),
-              Center(child: Text("Sign In", 
-              style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium!
-                    .copyWith(fontSize: 32),
-                    )
-                  ),
-              SizedBox(height: 20),
-              Text(
-                'Email',
-                style: Theme.of(context)
-                    .textTheme
-                    .labelSmall!
-                ),
-             TextField(
-              controller: nameController,
-            ),
-            nameControllerEmpty ? Text("email field can not be empty", style: Theme.of(context).textTheme.labelSmall!.copyWith(color: Colors.red),) : Container(),
-              SizedBox(height: 10),
-             Text(
-                  'Password',
-                  style: Theme.of(context)
-                    .textTheme
-                    .labelSmall!
-                ),
-              TextField(
-              controller: nameController1,
-              obscureText: true,
-            ),
-            nameController1Empty ? Text("password field can not be empty", style: Theme.of(context).textTheme.labelSmall!.copyWith(color: Colors.red),) : Container(),
-            SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                  });
-                  nameControllerEmpty = false;
-                  nameController1Empty = false;
-                  nameController.clear();
-                  nameController1.clear();
-                    
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpScreen()),
-                  );
-                },
-                style: ElevatedButton.styleFrom( 
-                  backgroundColor: Colors.white,
-                ),
-                child: Text('Sign Up', style: Theme.of(context).textTheme.labelMedium!.copyWith(color: Colors.blue) ,)
-              ),
-            ),
-            SizedBox(height: 10),
-             Row(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                    setState(() {
-                    });
-                    if (nameController.text.isEmpty || nameController1.text.isEmpty) {
-                      if (nameController.text.isEmpty && nameController1.text.isEmpty) {
-                        nameControllerEmpty = true;
-                        nameController1Empty = true;
-                      } else if (nameController.text.isEmpty) {
-                        nameControllerEmpty = true;
-                        nameController1Empty = false;
-                      } else if (nameController1.text.isEmpty) {
-                        nameController1Empty = true;
-                        nameControllerEmpty = false;
-                      }
-                       
-                      return;
-                    }
-                      AlertDialog alert = AlertDialog(
-                        title: Text("Login"),
-                        content: Text("Login Successful"),
-                      );
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return alert;
-                        },
-                      );
-                      nameControllerEmpty = false;
-                      nameController1Empty = false;
-                      nameController.clear();
-                      nameController1.clear();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 42, 57, 145),
-                    ),
-                    child: Text(
-                      'Login',
-                       style: Theme.of(context).textTheme.labelMedium!.copyWith(color: Colors.white),
-                    ),
+                Center(
+                  child: Image.network(
+                    'https://i.guim.co.uk/img/media/327aa3f0c3b8e40ab03b4ae80319064e401c6fbc/377_133_3542_2834/master/3542.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=34d32522f47e4a67286f9894fc81c863',
+                    height: 100,
                   ),
                 ),
-                const SizedBox(width: 10),
-                Expanded(
+                Center(
+                  child: Text(
+                    "Sign In",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium!
+                        .copyWith(fontSize: 32),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text('Email', style: Theme.of(context).textTheme.labelSmall),
+                TextFormField(
+                  controller: nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email field cannot be empty';
+                    } else if (!EmailValidator.validate(value)) {
+                      return 'Enter a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                Text('Password', style: Theme.of(context).textTheme.labelSmall),
+                TextFormField(
+                  controller: nameController1,
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password field cannot be empty';
+                    } else if (value.length < 7) {
+                      return 'Password must be at least 7 characters long';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                Center(
                   child: ElevatedButton(
                     onPressed: () {
+                      _resetForm();
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => ResetPasswordScreen()),
+                        MaterialPageRoute(builder: (context) => SignUpScreen()),
                       );
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.white,
                     ),
-                    child:  Text(
-                      'Reset password',
-                      style: Theme.of( context).textTheme.labelMedium!.copyWith(color: Colors.blue)
+                    child: Text(
+                      'Sign Up',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelMedium!
+                          .copyWith(color: Colors.blue),
                     ),
                   ),
                 ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            AlertDialog alert = AlertDialog(
+                              title: Text("Login"),
+                              content: Text("Login Successful"),
+                            );
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return alert;
+                              },
+                            );
+                            _resetForm();
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 42, 57, 145),
+                        ),
+                        child: Text(
+                          'Login',
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _resetForm();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ResetPasswordScreen()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                        ),
+                        child: Text(
+                          'Reset password',
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .copyWith(color: Colors.blue),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            ],
           ),
         ),
       ),
