17a806a62142b2f068f443106eb2bb6bc8e8f7f9
mixabyt <maksnot375@gmail.com>
2025-12-02 20:08:55 +0200

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar:  AppBar(
+          title: Text("Notes App"),
+        ),
         body: Notesscreen(),
       ),
     );
